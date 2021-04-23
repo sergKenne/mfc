073d75bae630dae8b5854423bc38536a6bcc1640
Serge Simplice Kenne <kenne_serge@mail.ru>
2021-04-23 17:08:07 +0300

    fixed sliderg

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 
 $(document).ready(function(){
-  $('.banner-slider').slick({
-    dots: true,
-    //infinite: true,
-    //speed: 300,
-    slidesToShow: 1,
-    arrows:false
-  });
-
-  // $('.news-slider').slick({
+  // $('.banner-slider').slick({
   //   dots: true,
   //   //infinite: true,
   //   //speed: 300,
   //   slidesToShow: 1,
   //   arrows:false
   // });
+  $('.banner-slider').bxSlider();
 
   $('.slider').bxSlider();
+
+  $('.partener-slider').bxSlider({
+    // maxSlides: 8,
+    // minSlides: 4
+
+    slideWidth: 170,
+    minSlides: 8,
+    maxSlides: 8,
+    slideMargin: 10
+  });
 });
