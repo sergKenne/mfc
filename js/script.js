16c3e6be2ef53199ac1ef675e305dd5d7d628b63
Serge Simplice Kenne <kenne_serge@mail.ru>
2021-04-26 18:27:01 +0300

    add some style

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 
 $(document).ready(function(){
-  // $('.banner-slider').slick({
-  //   dots: true,
-  //   //infinite: true,
-  //   //speed: 300,
-  //   slidesToShow: 1,
-  //   arrows:false
-  // });
   $('.banner-slider').bxSlider();
 
   $('.slider').bxSlider();
 
   $('.partener-slider').bxSlider({
-    // maxSlides: 8,
-    // minSlides: 4
-
     slideWidth: 170,
     minSlides: 8,
     maxSlides: 8,
-    slideMargin: 10
+    slideMargin: 10,
+    stopAutoOnClick: true,
   });
 });
