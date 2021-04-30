7e3b0b67ad646e53eb87f03adc37c72b7302cb57
Serge Simplice Kenne <kenne_serge@mail.ru>
2021-04-30 16:05:05 +0300

    fixed map

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
-
 $(document).ready(function(){
-  $('.banner-slider').bxSlider();
+    $('.banner-slider').bxSlider();
 
-  $('.slider').bxSlider();
+    $('.slider').bxSlider();
 
-  $('.partener-slider').bxSlider({
-    slideWidth: 170,
-    minSlides: 8,
-    maxSlides: 8,
-    slideMargin: 10,
-    stopAutoOnClick: true,
-  });
+    $('.partener-slider').bxSlider({
+      slideWidth: 170,
+      minSlides: 8,
+      maxSlides: 8,
+      slideMargin: 10,
+      stopAutoOnClick: true,
+    });
+
+    $("#search-js, #modal-btn-js").click(function(){
+      $("#modal-js").slideToggle(); 
+    });
+
+    $("#map-svg path").each(function(){
+      let color = $(this).attr("fill");
+        $(this).hover(
+            function(){
+              $(this).css({fill: "#F49678"})
+            }, function(){
+              $(this).css({fill: color})
+            }
+        );
+    })
 });
+
+
 
 
 
